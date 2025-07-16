ad3c40b114fcbcbd2473b8c999f9216920216220
Ali Mohamed <amx746@gmail.com>
2025-07-16 04:19:37 +0300

    Add file content hashing for change detection

--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -6,46 +6,25 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
-	"unicode"
+
+	"github.com/cespare/xxhash/v2"
 )
 
-// TODO
-// Handle --> IF the file Do not end by space (the normal)
-// It creates a new chunk just to last word
-// Two Syscalls per chunk (Read , Seek)
-// ---> Investigate another approach (May be use []byte to store left bytes from prev read)
-func ReadChunk(file *os.File, buffer []byte) ([]byte, error) {
-	n, err := file.Read(buffer)
-	if err != nil && err != io.EOF {
-		return nil, err
-	}
-	if n == 0 {
-		return nil, io.EOF
-	}
-	if err == io.EOF {
-		return buffer[:n], nil
-	}
-	splitPoint := findSplitPoint(buffer[:n])
+var Mp = make(map[string]map[int64]string)
 
-	// if splitPoint == -1 May be it is just huge text word
-	// Not Likely but if it happens just discard it for now
-	if splitPoint != -1 {
-		file.Seek(int64(splitPoint-n), io.SeekCurrent)
-		return buffer[:splitPoint], nil
-	}
-	return buffer[:n], nil
-}
+// I still need to save that in presistence storage
+var FileHash = make(map[string]uint64) // Save hashs of found files in index
+var mu sync.Mutex
 
-// TODO
-// Use Sync.Pool buffer (Better memory utilization)
 func StreamToChannel(chunkSize int, fileChan <-chan string, docChan chan<- *models.Document) error {
 	var wg sync.WaitGroup
 	numWorkers := 4
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go func(workerID int, buffer []byte) {
+		go func(workerID int, buffer []byte, content *strings.Builder) {
 			defer wg.Done()
 			for filePath := range fileChan {
 				file, err := os.Open(filePath)
@@ -53,43 +32,43 @@ func StreamToChannel(chunkSize int, fileChan <-chan string, docChan chan<- *mode
 					fmt.Printf("Error in Opening file %v , Worker %v", err, workerID)
 					continue
 				}
+				fmt.Println("Reader: ", file.Name())
 				info, err := file.Stat()
 				if err != nil {
 					fmt.Println(filePath)
 					continue
 				}
+				fileHash := xxhash.New()
+				for {
+					n, err := file.Read(buffer)
+					if err != nil && err != io.EOF {
+						print(err)
+					}
+					if err == io.EOF {
+						break
+					}
+					fileHash.Write(buffer[:n])
+					content.Write(buffer[:n])
+				}
+				currHash := fileHash.Sum64()
+				mu.Lock()
+				// If the Hash is the same
+				// Negative Write (Just AutoSave and the content is the same)
+				if hash, ok := FileHash[filePath]; ok && hash == currHash {
+					mu.Unlock()
+					continue
+				}
+				FileHash[filePath] = currHash
+				mu.Unlock()
 				ext := filepath.Ext(filePath)
 				modtime := info.ModTime().Format(time.RFC1123)
 				size := uint64(info.Size())
-				for {
-					content, err := ReadChunk(file, buffer)
-					if err != nil {
-						if err != io.EOF {
-							fmt.Println(filePath)
-						}
-						break
-					}
-					docChan <- models.NewDocument(
-						filePath,
-						size,
-						modtime,
-						ext,
-						string(content))
-				}
+				docChan <- models.NewDocument(filePath, size, modtime, ext, content.String())
+				content.Reset()
 				file.Close()
 			}
-		}(i, make([]byte, chunkSize))
+		}(i, make([]byte, chunkSize), &strings.Builder{})
 	}
 	wg.Wait()
 	return nil
-}
-
-func findSplitPoint(buffer []byte) int {
-	maxPos := len(buffer) - 1
-	for i := maxPos; i > 0; i-- {
-		if unicode.IsSpace(rune(buffer[i])) || unicode.IsPunct(rune(buffer[i])) {
-			return i + 1
-		}
-	}
-	return -1
 }
