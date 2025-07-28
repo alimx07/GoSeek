dfc6358358138f481438925dc844aff2df7f07b5
Ali Mohamed <amx746@gmail.com>
2025-07-29 02:31:57 +0300

    Add New fields per document
    Dir : for using in Search filtering
    
    Size: For Search results

--- internal/models/document.go
+++ internal/models/document.go
@@ -1,11 +1,15 @@
 package models
 
+import "path/filepath"
+
 type Document struct {
-	Path      string `json:"path"`
-	Size      int64  `json:"size"`
-	ModTime   string `json:"mod_time"`
-	Extension string `json:"extension"`
-	Content   string `json:"content"`
+	Path      string  `json:"path"`
+	Dir       string  `json:"dir"`
+	Size      int64   `json:"size"`
+	Score     float64 `json:"score"`
+	ModTime   string  `json:"mod_time"`
+	Extension string  `json:"extension"`
+	Content   string  `json:"content"`
 }
 
 // Returns New Document object
@@ -13,6 +17,7 @@ func NewDocument(path string, size int64, modTime string, extension string, cont
 
 	return &Document{
 		Path:      path,
+		Dir:       filepath.Dir(path),
 		Size:      size,
 		ModTime:   modTime,
 		Extension: extension,
